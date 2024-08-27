414897926df47b980a8c3c96c206f38afe7301a6
VZ1308 <154434964+VZ1308@users.noreply.github.com>
2024-08-27 08:52:34 +0200

    Update Main.java
    edit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Benutzer zur Eingabe einer Zeichenkette auffordern
+        System.out.println("Bitte geben Sie die Zeichenkette im Format -derArtikel[Artikelbezeichnung]wurdebestelltvon[besteller'in]- ein:");
+        String input = scanner.nextLine();
+
+        // Schließen des Scanners
+        scanner.close();
+
+        // Artikelbezeichnung und Besteller extrahieren
+        String articlePrefix = "derArtikel[";
+        String orderedByPrefix = "wurdebestelltvon[";
+        String articleSuffix = "]";
+        String orderedBySuffix = "]";
+
+        // Überprüfen, ob die Eingabe das richtige Format hat
+        if (input.contains(articlePrefix) && input.contains(orderedByPrefix)) {
+            // Extrahieren der Artikelbezeichnung
+            int articleStartIndex = input.indexOf(articlePrefix) + articlePrefix.length();
+            int articleEndIndex = input.indexOf(articleSuffix, articleStartIndex);
+            String article = input.substring(articleStartIndex, articleEndIndex);
+
+            int orderedByStartIndex = input.indexOf(orderedByPrefix) + orderedByPrefix.length();
+            int orderedByEndIndex = input.indexOf(orderedBySuffix, orderedByStartIndex);
+            String orderedBy = input.substring(orderedByStartIndex, orderedByEndIndex);
+
+            System.out.println("Artikelbezeichnung: " + article);
+            System.out.println("Besteller: " + orderedBy);
+        } else {
+            System.out.println("Die Eingabe entspricht nicht dem erwarteten Format.");
         }
     }
 }
